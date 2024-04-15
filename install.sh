06c4dd8d45a0e3b7bdcac5e1563207ef4115d9ef
extremeoc <51958683+extremeoc@users.noreply.github.com>
2024-04-15 23:45:52 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@
 
 # Downloading xray_service
 echo "Downloading xray_service..."
-wget -q https://raw.githubusercontent.com/mohamadkhalaj/extremeoc/main/xray_service -O /etc/init.d/xray_service
-wget -q https://raw.githubusercontent.com/mohamadkhalaj/extremeoc/main/xray_service.sh -O /root/xray_service.sh
+wget -q https://raw.githubusercontent.com/extremeoc/xray-service/main/xray_service -O /etc/init.d/xray_service
+wget -q https://raw.githubusercontent.com/extremeoc/xray-service/main/xray_service.sh -O /root/xray_service.sh
 
 # Set executable permissions
 chmod +x /etc/init.d/xray_service /root/xray_service.sh
